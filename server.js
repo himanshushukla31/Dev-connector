0ed423cf832ccbc9ac5a5367a54fef19cc008ba3
himanshushukla31 <72408932+himanshushukla31@users.noreply.github.com>
2021-07-13 08:35:03 +0530

    Inital commit to DevConnector

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('API running')
+})
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(
+  PORT,
   console.log(
     `Successfully running server in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
-})
+)
